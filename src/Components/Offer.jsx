68ca9a074aec4e6f814b660be1a88e26547aa638
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-06-10 10:03:58 +0100

    quickselar

--- src/Components/Offer.jsx
+++ src/Components/Offer.jsx
@@ -6,14 +6,14 @@ function Offer() {
 
         <div className='flex flex-col items-center justify-center'>
           <h1 className='text-md text-blue-900 mb-2 tracking-wide'>WE OFFER BEST IT SERVICES</h1>
-          <h1 className='text-3xl font-bold tracking-wider mb-5'>Easy Solutions for all Difficult IT Problems</h1>
+          <h1 className='text-3xl font-bold tracking-wider mb-5 text-center'>Easy Solutions for all Difficult IT Problems</h1>
 
-          <div className="flex">
-
+          <div className="flex items-center space-x-4 border-b border-gray-600 ">
+            <h1 className="font-light tracking-wide">Technology Index</h1>
           </div>
         </div>
 
-        <div className="">
+        <div className="flex">
 
         </div>
 
