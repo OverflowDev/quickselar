43e8f0fe044386e12ddc048073f8cf17a229a3ce
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-06-10 09:59:55 +0100

    quickselar

--- src/Components/Offer.jsx
+++ src/Components/Offer.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
 function Offer() {
   return (
-    <div>Offer</div>
+    <div className='bg-gray-100'>
+      <div className="md:max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+
+        <div className='flex flex-col items-center justify-center'>
+          <h1 className='text-md text-blue-900 mb-2 tracking-wide'>WE OFFER BEST IT SERVICES</h1>
+          <h1 className='text-3xl font-bold tracking-wider mb-5'>Easy Solutions for all Difficult IT Problems</h1>
+
+          <div className="flex">
+
+          </div>
+        </div>
+
+        <div className="">
+
+        </div>
+
+      </div>
+
+
+    </div>
   )
 }
 
