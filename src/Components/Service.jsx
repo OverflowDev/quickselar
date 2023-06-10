3243d7a96cda778d8a01a9306b486285a5631630
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-06-10 09:04:04 +0100

    quickselar

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
 
 function Service() {
   return (
-    <div>Service</div>
+    <div>
+      <div className='md:flex items-center justify-between md:max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
+          <div className='animate-fadeInUp delay-200 duration-400 md:w-2/5 ' >
+            <h1 className='text-lg text-blue-800 mb-2 tracking-normal font-light'>OUR SERVICE</h1>
+            <h2 className='text-gray-800 text-3xl font-semibold tracking-wide mb-8'>
+            We Offer a Wide Variety of IT Services
+            </h2>
+          </div>
+          <div className=''>
+            <button className='flex items-center justify-center bg-blue-700 text-white py-2 px-4 w-36 h-12 rounded-lg'>
+              <span>See More</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+              </svg>
+            </button>
+          </div>
+      </div>
+    </div>
   )
 }
 
--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from '../assets/logo.png'
 function Navbar() {
   return (
     // <div className='fixed top-0 inset-x-0'>
-    <div className='sticky top-0'>
+    <div className='sticky top-0 z-10'>
         <nav className="flex justify-between items-center text-center py-4 md:px-24 px-8 bg-gray-100 bg-main bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 shadow sm:items-baseline w-full">
             <div className="">
                 <img src={Logo} alt="logo" className='bg-cover w-full h-4' />
