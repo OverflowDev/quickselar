7d3b4c2f106d13ad2ac73be74f38b15bc11c5b44
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-06-10 09:36:37 +0100

    quickselar

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -22,7 +22,8 @@ function Service() {
       <div className=" md:max-w-screen-xl px-4 mt-3 mx-auto sm:px-6 lg:px-8">
         <div className="flex flex-row gap-8">
           
-          <div className="flex-col md:w-1/3 h-48 bg-white group hover:bg-blue-600 hover:text-white rounded-lg animate-fadeInUp delay-100 duration-100 px-6 py-2">
+          <div className="flex-col md:w-1/3 h-48 bg-white group hover:bg-blue-600 hover:text-white rounded-lg animate-fadeInUp delay-100 duration-100 transition-all delay-200 px-6 py-2">
+            
             <div className="flex items-center justify-center space-x-4 text-2xl group-hover:text-white text-blue-800 font-semibold tracking-wide">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
